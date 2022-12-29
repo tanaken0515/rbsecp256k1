2ee3bed5ce81fec12db4acd7982a5ddba7b0b9bb
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2022-12-29 20:45:08 +0900

    debug

--- lib/rbsecp256k1.rb
+++ lib/rbsecp256k1.rb
@@ -7,4 +7,11 @@ end
 require 'rbsecp256k1/context'
 require 'rbsecp256k1/util'
 require 'rbsecp256k1/version'
+
+puts '*' * 300
+Dir.foreach('./rbsecp256k1') do |item|
+  puts item
+end
+puts '*' * 300
+
 require 'rbsecp256k1/rbsecp256k1'
